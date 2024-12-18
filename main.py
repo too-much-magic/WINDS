a91ba8363fb09102660ed27367ad1d80095228a2
too-much-magic <dylancrombie@outlook.com>
2024-12-18 13:20:47 -0700

    Added API key security.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import customtkinter
+import requests
+import os
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -22,10 +24,35 @@ city = customtkinter.CTkEntry(master=search_container, placeholder_text="City")
 city.pack(pady=12, padx=10,side='left')
 
 def search():
-    text = city.get()
+    api_key = os.getenv("WEATHER_API_KEY")
+
+    if not api_key:
+        raise ValueError("API key is not set.")
+    
+    params = {
+        'q' : city.get(),
+        'key' : api_key,
+        'aqi' : "yes",
+        'days' : '7' # TODO: const for now, could change later
+    }
+    base_url = "http://api.weatherapi.com/v1/forecast.json"
+        
     for element in {title, subtitle, search_container}:
         element.destroy()
     frame.pack(pady=20, padx=60, expand=True, fill="both")
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()
+        
+        weather_data = response.json()
+        
+        location = customtkinter.CTkLabel(master=frame, text=f"WINDS for {weather_data["location"]["name"]}:", font=("Roboto", 24) )
+        location.pack(pady=12, padx=10)
+        
+        
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
     
     
 search_button = customtkinter.CTkButton(master=search_container, text="Search!", command=search)
